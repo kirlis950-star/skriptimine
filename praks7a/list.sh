d40efd4d0eb6f24d03c763ce2d8b8f9e4c06a71e
kirlis950-star <renee-martin.oim@voco.ee>
2024-09-18 09:57:24 +0300

    nüüd loendab ka veel

--- praks7a/list.sh
+++ praks7a/list.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 #skript, mis kontrollib, kas see on fail, link või kataloog
 #kontrollib praegust
+failid=0
+kataloogid=0
+linkid=0
+
  for i in `ls -l`
  do
    if test -f $i; then
      echo $i: fail
+     failid=$((failid + 1))
  elif test -L $i; then
      echo $i: link
+     linkid=$((linkid + 1))
  elif test -d $i; then
      echo $i: kataloog
+     kataloogid=$((kataloogid + 1))
  fi
  done
+
+# Väljastame kokkuvõtte failide, kataloogide ja linkide arvust.
+echo "Kokku on $failid faili, $kataloogid kataloogi ja $linkid linki."
